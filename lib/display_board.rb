fa3fa2167cd7c8d471a1e162b91ad54fb3392e85
afox@wework.com <afox@wework.com>
2018-02-17 18:59:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
+board = [" "," "," "," "," "," "," "," "," "]
 
-
-def display_board
+def display_board(arg)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
+
+display_board(board)
